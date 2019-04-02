4e8f6a98a50c7f0ad59d3715d81dc9a8df4ec6c7
stephencreel <46076458+stephencreel@users.noreply.github.com>
2019-04-01 20:45:11 -0500

    Create reverse-integer.cpp

---
+++ reverse-integer.cpp
@@ -0,0 +1,32 @@
+/*/
+https://leetcode.com/problems/reverse-integer/
+
+Program reverses an input integer and returns 0 if integer overflow occurs.
+
+Runtime: 4 ms, faster than 100.00% of C++ online submissions for Reverse Integer.
+Memory Usage: 8 MB, less than 100.00% of C++ online submissions for Reverse Integer.
+(4/1/2019)
+/*/
+
+class Solution {
+public:
+    int reverse(int number) {
+        
+        int solution = 0;
+        while (number != 0) {
+            
+            // Remove Least Significant Digit from Input and Store
+            int digit = number % 10;
+            number = (number - digit) / 10;
+            
+            // Add Digit to Solution
+            long long temp = solution;
+            temp = (temp * 10) + digit;
+            
+            // Return 0 if Integer Overflow
+            if(temp > INT_MAX || temp < INT_MIN) return 0;
+            solution = temp;
+        }
+        return solution;
+    }
+};

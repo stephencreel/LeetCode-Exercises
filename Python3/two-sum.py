d5d4d800c2dd2440f01e5bbe760eca1faf7cb5c3
stephencreel <46076458+stephencreel@users.noreply.github.com>
2019-05-26 21:28:41 -0500

    Update two-sum.py

--- Python3/two-sum.py
+++ Python3/two-sum.py
@@ -1,12 +1,10 @@
-'''
-https://leetcode.com/problems/two-sum/
-
-Program returns indices of numbers in list that add to input target number.
-
-Runtime: 32 ms, faster than 99.21% of Python3 online submissions for Two Sum.
-Memory Usage: 14.5 MB, less than 23.17% of Python3 online submissions for Two Sum.
-(5/26/2019)
-'''
+# https://leetcode.com/problems/two-sum/
+#
+# Program returns indices of numbers in list that add to input target number.
+#
+# Runtime: 32 ms, faster than 99.21% of Python3 online submissions for Two Sum.
+# Memory Usage: 14.5 MB, less than 23.17% of Python3 online submissions for Two Sum.
+# (5/26/2019)
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:  
